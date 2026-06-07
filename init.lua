ea727bb091587d981c11f6c97743eebcd34d2e7b
Juliano Soares da Silva <bytebytebug@gmail.com>
2026-06-07 12:39:01 -0300

    add basic init.lua

---
+++ init.lua
@@ -0,0 +1,16 @@
+
+
+-- basic configs
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+vim.opt.breakindentopt = { "shift:4" }
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.showbreak = "↳"
+
